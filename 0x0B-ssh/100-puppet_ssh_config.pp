5ec2a3ad79cc836904e21059da9214d520c8de52
Brig1tte <korantengbrigitte96@gmail.com>
2023-11-24 22:01:56 -0800

    SSH Config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,18 @@
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN== 398531-web-01
+#!/usr/bin/env bash
+#Code to use puppet to connect without password
+
+file { '/etc/ssh/ssh_config':
+  ensure -> present,
+}
+
+file_line { 'Turn off passwd auth':
+  path    -> '/etc/ssh/ssh_config',
+  line    -> 'PasswordAuthentication no',
+  match   -> '^#PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path    -> '/etc/ssh/ssh_config',
+  line    -> 'IdentityFile ~/.ssh/school',
+  match   -> '^#IdentityFile'
+}
